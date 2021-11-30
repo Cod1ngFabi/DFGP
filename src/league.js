da59d73a3f0dd54ddd986f1bc4d81a057a629044
Fabian Biederer <biedererfabian@gmx.de>
2021-11-30 20:44:07 +0100

    added Counter

--- src/league.js
+++ src/league.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
+import React, { useState, useEffect, useRef } from 'react';
 
 const League = () => {
     return (
@@ -12,7 +13,7 @@ const League = () => {
                         <p>Ostbayern 2021/2022</p>
                         <p>Kreisliga Deggendorf/Straubing</p>
                         <p>Zur aktuellen <a href="https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=DVO+2021%2F22&group=209483" target="_blank">Tabelle</a></p>
-                        <p>Downloads: <a href="././img/Spielplan.pdf" download="Spielplan.pdf" target="_blank">Aktueller Spielplan</a> | <a href="././img/Kalender.ics" download="Kalender.ics" target="_blank">Kalender</a></p>
+                        <p>Downloads: <a href="././img/Spielplan.pdf" download="Spielplan.pdf" target="blank">Aktueller Spielplan</a> | <a href="././img/Kalender.ics" download="Kalender.ics" target="blank">Kalender</a></p>
                     </Col>
                     <Col>
                         <Counter />
@@ -25,12 +26,38 @@ const League = () => {
 
 
 const Counter = () => {
+    const label = "Anzahl Aktiver";
+    const number = 0;
+    const duration = 4;
+    const hiddenRef = useRef();
+    const [count, setCount] = useState(0);
+    
+    useEffect(() => {
+        let start = 0;
+        const end = 15;
+        
+            if (start === end){
+                return;
+            } 
+     
+            let totalMilSecDur = duration;
+            let incrementTime = (totalMilSecDur / end) * 1000;
+            
+            let timer = setInterval(() => {
+                start += 1;
+                setCount(start + number);
+                if(start === end) {
+                    clearInterval(timer);
+                    return;
+                }
+            }, incrementTime)
+        
+    }, [number, duration]);
+    
     return (
         <div className="Count">
           <h3>
-              Anzahl Spielberechtigter:
-            <i></i>
-            
+            <i>{label}: {count}</i>
           </h3>
         </div>
       )
