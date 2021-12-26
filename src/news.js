a2e4557b14641343f8f149a0fd8aed5f1f32fba2
Fabian Biederer <biedererfabian@gmx.de>
2021-12-26 14:47:29 +0100

    removed width in iframe

--- src/news.js
+++ src/news.js
@@ -10,7 +10,7 @@ const News = () => {
                     <h2 style={{textAlign: "center"}}>Folge uns</h2>
                     <center>
                         <iframe className="newsFeed" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDartfreunde-Geisling-Pfatter-eV-102601505575227&tabs=timeline&width=500&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
-                        width="500" height="600" scrolling="no" frameborder="0" allowfullscreen="true" 
+                        height="600" scrolling="no" frameborder="0" allowfullscreen="true" 
                         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                     </center>
                 </Row>
