7c6a5091653b60de1d3e1745a8865f7c4fb1139c
Fabian Biederer <biedererfabian@gmx.de>
2021-12-27 14:42:27 +0100

    created complete imprint

--- src/imprint.js
+++ src/imprint.js
@@ -8,16 +8,28 @@ import App from './App.js';
 class Imprint extends React.Component{
     render(){
         return (
-                <Container>
+                <Container style={{padding: 50 + "px"}}>
                     <Row>
                         <Col>
+                            <p><Link to="/" element={<App />}>Zurück zur Home Seite</Link></p>
                             <h1>Impressum</h1>
+                            <p>Die Website der Dartfreunde Geisling-Pfatter muss nicht immer die Meinung der Verantwortlichen wiedergeben. Sollten Berichte oder insbesondere Fotos veröffentlicht werden, die betreffende Personen aus ihrer Sicht negativ darstellen und deshalb entfernt werden sollten, so wenden sie sich an die Webmaster. Die Dartfreunde Geisling-Pfatter haben keinerlei Einfluss auf den Inhalt und die Gestaltung von verlinkten Seiten auf dieser Homepage und distanziert sich ausdrücklich davon.</p>
                             <br />
-                            <p>Dartfreunde Geisling-Pfatter e.V.</p>
-                            <p>93102 Geisling</p>
-                            <p>E-Mail: DartfreundeGeislingPfatter@gmx.de</p>
-                            <p>Tel: 2001705876077</p>
-                            <button><Link to="/" element={<App />}>Zurück zur Home Seite</Link></button>
+                            <p>Dartfreunde Geisling-Pfatter e.V. <br /> 93102 Geisling <br /> E-Mail: DartfreundeGeislingPfatter@gmx.de <br />Tel: 2001705876077</p>
+                            <p>Webmaster: Biederer Fabian <br /> Verantwortlich für den Internetauftritt: Biederer Fabian <br />E-mail: biedererfabian@gmx.de</p>
+                            <br />
+                            <p><b>Haftungsausschluss / Disclaimer</b></p>
+                            <p>1. Inhalt des Onlineangebotes<br />
+                            Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt. Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.</p>
+                            <p>2. Verweise und Links<br />
+                            Bei direkten oder indirekten Verweisen auf fremde Internetseiten ("Links"), die außerhalb des Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern. Der Autor erklärt daher ausdrücklich, dass zum Zeitpunkt der Linksetzung die entsprechenden verlinkten Seiten frei von illegalen Inhalten waren. Der Autor hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte der gelinkten/verknüpften Seiten. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller gelinkten /verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor eingerichteten Gästebüchern, Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.</p>
+                            <p>3. Urheber- und Kennzeichenrecht<br />
+                            Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihm selbst erstellte Grafiken, Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente, Videosequenzen und Texte zurückzugreifen. Alle innerhalb des Internetangebotes genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass Markenzeichen nicht durch Rechte Dritter geschützt sind! Das Copyright für veröffentlichte, vom Autor selbst erstellte Objekte bleibt allein beim Autor der Seiten. Eine Vervielfältigung oder Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in anderen elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung des Autors nicht gestattet.</p>
+                            <p>4. Datenschutz<br />
+                            Sofern innerhalb des Internetangebotes die Möglichkeit zur Eingabe persönlicher oder geschäftlicher Daten (Emailadressen, Namen, Anschriften) besteht, so erfolgt die Preisgabe dieser Daten seitens des Nutzers auf ausdrücklich freiwilliger Basis. Die Inanspruchnahme und Bezahlung aller angebotenen Dienste ist - soweit technisch möglich und zumutbar - auch ohne Angabe solcher Daten bzw. unter Angabe anonymisierter Daten oder eines Pseudonyms gestattet.</p>
+                            <p>5. Rechtswirksamkeit dieses Haftungsausschlusses<br />
+                            Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.</p>
+                            <p><Link to="/" element={<App />}>Zurück zur Home Seite</Link></p>
                         </Col>
                     </Row>
                 </Container>
