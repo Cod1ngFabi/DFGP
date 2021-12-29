22a2ca45fb24ca60c5499bc12f9164d4cefbf314
Fabian Biederer <biedererfabian@gmx.de>
2021-12-29 18:41:05 +0100

    added Not Found Site to the Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Imprint from './imprint';
+import NotFound from './notFound';
 import { Route, Routes, BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,6 +12,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />}/>
       <Route path="/Impressum" element={<Imprint />}/>
+      <Route path="*" element={<NotFound />}/>
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
