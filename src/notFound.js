68ab28f90f04f585b79474bd7d856e8a417ecc54
Fabian Biederer <biedererfabian@gmx.de>
2021-12-29 18:39:12 +0100

    little text update

--- src/notFound.js
+++ src/notFound.js
@@ -5,7 +5,7 @@ import Imprint from './imprint.js';
 const NotFound = () => {
     return (
         <div style={{padding: 50 + "px"}}>
-            <h2>404 Nicht gefunden</h2>
+            <h2>404: Seite nicht gefunden</h2>
             <p>Es tut uns leid, aber die von Ihnen gesuchte Seite existiert nicht.</p>
             <p><Link to="/Impressum" element={<Imprint />}>Meinten Sie /Impressum ?</Link></p>
         </div>
